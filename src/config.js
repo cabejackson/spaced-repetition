8c12b32ffaa5838481b4c44c563c15bf6a16f878
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-11-13 11:32:21 -0500

    fixed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://pure-anchorage-76716.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
